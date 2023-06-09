3a91c0b728aa8877acd8f1f7cddb657930993489
rylCodes <andresdaryl.94@gmail.com>
2023-06-09 21:15:10 +0800

    improve mentor dynamic page

--- nc_app/views.py
+++ nc_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import BusinessType, Expert, Education, Experience, Detail
-from django.shortcuts import get_object_or_404
+from .models import BusinessType, Expert
+from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
@@ -9,11 +9,13 @@ def home(request):
     }
     return render(request, 'home.html', context)
 
+
 def about(request):
     context = {
         'page_title': 'About'
     }
     return render(request, 'about.html', context)
+
 
 def resources(request):
     context = {
@@ -21,11 +23,13 @@ def resources(request):
     }
     return render(request, 'resources.html', context)
 
+
 def assistance(request):
     context = {
         'page_title': 'Assistance'
     }
     return render(request, 'assistance.html', context)
+
 
 def registration(request):
     business_types = BusinessType.objects.all()
@@ -35,11 +39,13 @@ def registration(request):
     }
     return render(request, 'registration.html', context)
 
+
 def contact(request):
     context = {
         'page_title': 'Contact',
     }
     return render(request, 'contact.html', context)
+
 
 def faqs(request):
     content = {
@@ -47,24 +53,32 @@ def faqs(request):
     }
     return render(request, 'faqs.html', content)
 
+
 def advisory(request):
-    filter_value = request.GET.get('filter')
     experts = Expert.objects.all()
+    filter_value = request.GET.get('filter')
+    #experts_per_page = 8
+    #paginator = Paginator(experts, experts_per_page)
 
     if filter_value:
-        experts = experts.filter(expertise=filter_value)
+        experts = experts.filter(expertise__contains=filter_value)
 
     content = {
         'page_title': 'Business Advisory',
         'experts': experts,
-        'selected_filter': filter_value  # Add this line to pass the selected filter to the template
+        'selected_filter': filter_value
     }
     return render(request, 'advisory.html', content)
 
+
 def expert_mentor(request, id_name):
     mentors = Expert.objects.get(id_name=id_name)
+    expertise = mentors.expertise
+    related_mentors = Expert.objects.filter(expertise__contains=expertise).exclude(id_name=id_name)
+    
     content = {
         'mentors': mentors,
+        'related_mentors': related_mentors,
         'page_title': 'Expert Mentors'
     }
     return render(request, 'expert_mentor.html', content)
--- static/js/advisory.js
+++ static/js/advisory.js
@@ -1,4 +1,4 @@
-function addFilter() {
+function toggleAddFilter() {
     var filterOption = document.getElementById("filterOption");
     filterOption.classList.toggle("hidden");
     filterOption.classList.toggle("flex");
