fb879e97c176abc16ef7e1e357a06d5a6255f837
rylCodes <andresdaryl.94@gmail.com>
2024-01-04 15:46:26 +0800

    hosting on vercel.com

--- nc_web/settings.py
+++ nc_web/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-nidqmhf*cc#kv4o0v$uq=-winrd+@#@w&#+njkde3!!-w7ib+z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 # Application definition
 
